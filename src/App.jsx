baf7014545938e42c1b48286ca4c0740922da95c
arttu <ttu91@hotmail.com>
2024-02-07 16:15:50 +0200

    Make more colorful

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ const ShowPage = ({ name }) => {
         <div className="pl-2 sm:pl-4">
           <b>Technologies used:</b>
           <div className="flex flex-row gap-2 text-sm flex-wrap my-2 justify-around">
-            {technologies.map(t => <div key={t} className="bg-sky-200 p-1">{t}</div>)}
+            {technologies.map(t => <div key={t} className="bg-orange-400 p-1">{t}</div>)}
           </div>
         </div>
       </div>
@@ -42,7 +42,7 @@ function App() {
   return (
     <>
       <div className="max-w-screen-lg mx-auto pb-4 break-words">
-        <div className="bg-gradient-to-br from-stone-500 to-stone-100 via-stone-200 flex flex-wrap justify-between rounded-b-md transition">
+        <div className="bg-gradient-to-br from-orange-300 to-amber-300 via-orange-500 flex flex-wrap justify-between rounded-b-md transition">
           <div className="my-auto"><h1 className="p-4">Arttu Kangas - Portfolio</h1></div>
           <div className="my-auto p-4">
             <ul>
@@ -65,11 +65,11 @@ function App() {
         <div className="flex flex-col gap-4 pt-2 flex-row sm:flex-row"><h2 className="font-bold my-auto">Projektit</h2>
           <div className="flex flex-row gap-4">
             {pages.map(name => (
-              <div key={name} className={`${buttonStyle} ${name === page ? 'bg-stone-200' : 'bg-stone-100'}`} onClick={() => setPage(name)}>{name}</div>
+              <div key={name} className={`${buttonStyle} ${name === page ? 'bg-orange-200' : 'bg-stone-200'}`} onClick={() => setPage(name)}>{name}</div>
             ))}
           </div>
         </div>
-        <div className="bg-gradient-to-b from-stone-200 to-white p-2">
+        <div className="bg-gradient-to-b from-orange-200 to-amber-50 p-2">
           <ShowPage name={page} />
         </div>
       </div>
